c3d31ff7f775a73095afbb55941fddb0b3b54bca
TakumaAkahane <akahane.t@isoroot.jp>
2019-04-23 18:47:16 +0900

    adapt to Zendframework + Smarty on Docker

--- zftutorial/public/index.php
+++ zftutorial/public/index.php
@@ -9,6 +9,10 @@ use Zend\Stdlib\ArrayUtils;
  */
 chdir(dirname(__DIR__));
 
+require(__DIR__. '/../Smarty/configs/MySmarty.php');
+$smarty = new MySmarty();
+$smarty->display('index.tpl');
+
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
